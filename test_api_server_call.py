57b47a8d36a80991dff428a043d747632ef45056
Santanu Saha <50931396+psantanusaha@users.noreply.github.com>
2020-08-24 23:07:28 -0700

    Update test_api_server_call.py

--- test_api_server_call.py
+++ test_api_server_call.py
@@ -58,8 +58,9 @@ def do_signed_request(key_id, key_secret, host_url, method, resource_path, conte
     else:
         raise Exception("Unsupport method provided, cannot perform a signed request.")
 
-def api_get_auth_headers(method, discovery_ds_id='', resource_path='', content_type=None, content=None, upload_type='success', spool_subfolder='', amz_headers={}):
-    
+
+def api_get_auth_headers(method, discovery_ds_id='', resource_path='', content_type=None, content=None,
+                         upload_type='success', spool_subfolder='', amz_headers={}):
     headers = {}
 
     post_body = {}
@@ -72,30 +73,25 @@ def api_get_auth_headers(method, discovery_ds_id='', resource_path='', content_t
     post_body['content'] = content
     post_body['amz_headers'] = amz_headers
 
-    try:
-        params = get_global_parameters()
-    except RedisConnectionFailure:
-        return headers
-
-    print params
     print post_body
-    
+
     # agent_key_id = params['agent_key_id']
     # agent_key_secret = params['agent_key_secret']
     # api_endpoint = params['api_endpoint']
-    # tenant_db = params['tenant_domain']  
+    # tenant_db = params['tenant_domain']
 
     agent_key_id = "3f5bf8ace65111eab0d202e3173504a7"
     agent_key_secret = "c2AD04Gee34RkXHw8wU2Rt3fYbvLhYGBa0q6fvH7j8U"
     api_endpoint = "https://api-eoe.elastica-inc.com"
     tenant_db = "elasticaco"
-     
 
     if not (api_endpoint and tenant_db and agent_key_id and agent_key_secret):
         # return empty headers, request will fail.
         return headers
 
-    resp = do_signed_request(agent_key_id, agent_key_secret, api_endpoint+"/", "POST", "/"+tenant_db+"/api/admin/v1/sign_request", content_type="application/json", content=json.dumps(post_body))
+    resp = do_signed_request(agent_key_id, agent_key_secret, api_endpoint + "/", "POST",
+                             "/" + tenant_db + "/api/admin/v1/sign_request", content_type="application/json",
+                             content=json.dumps(post_body))
 
     if resp.status_code == 200:
         result = json.loads(resp.text)
@@ -105,33 +101,33 @@ def api_get_auth_headers(method, discovery_ds_id='', resource_path='', content_t
     return (headers, '')
 
 
-def testing_api_call() :
-    upload_ctx = {'discovery_ds_id' : 'some_dummy_id'}
+def testing_api_call():
+    upload_ctx = {'discovery_ds_id': 'some_dummy_id'}
     source_file_path = "/tmp/test_file_1.gz"
     file_name = source_file_path
 
     slash_index = source_file_path.rfind('/')
     if slash_index != -1:
-        file_name = source_file_path[slash_index+1:]
+        file_name = source_file_path[slash_index + 1:]
 
     file_name = urllib.quote_plus(file_name)
 
     mime = magic.Magic(mime=True)
     file_content_type = mime.from_file(source_file_path)
-    
 
     amz_headers = {}
-    (headers, dest_url) = api_get_auth_headers("POST", upload_ctx.get('discovery_ds_id', ''), file_name+"?uploads", "application/json",
-                                 upload_type=upload_ctx.get('upload_type', 'success'), spool_subfolder=upload_ctx.get('spool_subfolder'), amz_headers=amz_headers)
+    (headers, dest_url) = api_get_auth_headers("POST", upload_ctx.get('discovery_ds_id', ''), file_name + "?uploads",
+                                               "application/json",
+                                               upload_type=upload_ctx.get('upload_type', 'success'),
+                                               spool_subfolder=upload_ctx.get('spool_subfolder'),
+                                               amz_headers=amz_headers)
 
     print headers
     print dest_url
 
-    resp = requests.post(dest_url+file_name+"?uploads", headers=headers, data=None)
+    resp = requests.post(dest_url + file_name + "?uploads", headers=headers, data=None)
     print resp.text
-
 
 
 if __name__ == "__main__":
     testing_api_call()
-
